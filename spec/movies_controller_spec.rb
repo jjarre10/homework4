22477d6ee31164ba2a378dc622705ae0d67bc12d
Jonathan Jarrett <jjarre10@uncc.edu>
2014-04-25 19:30:41 -0300

    Initial commit

---
+++ spec/movies_controller_spec.rb
@@ -0,0 +1,7 @@
+describe MoviesController do
+  before :each do #Runs before each test
+    @movie = Movie.new {:title=> "Star Wars", :rating=> "PG", :release_date=> "1977-05-25"} 
+  end
+  
+  #describe 
+end
