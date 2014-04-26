5e35ccbb377cd428453146e969ac7e1fd3d681e4
Jonathan Jarrett <jjarre10@uncc.edu>
2014-04-25 23:22:38 -0300

    Got the edit page test to pass

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,13 +1,10 @@
-Given /The following movies exist/ do |table|
-    table.hashes.each do
-      Movie.create!(movie)
-    end
-
+Given(/^the following movies exist:$/) do |table|
+  table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
 Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
   #arg1.should_be(director=>arg2)
   movie = Movie.find_by_title(arg1)
   assert arg2 == movie.director
-end
-
-
 end
