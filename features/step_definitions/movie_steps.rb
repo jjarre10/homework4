84108050332a914417e781d886140153df686121
Dylan Bailey <dylancarl@live.com>
2014-04-28 12:04:49 -0400

    added empty cucumber step definitions; made a tempplate to turn into rspec

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -8,3 +8,40 @@ Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
   movie = Movie.find_by_title(arg1)
   assert arg2 == movie.director
 end
+
+When(/^I go to the (.*?) for "(.*?)"$/) do |movie|
+  path_to(movie)
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the details page for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be on the Similar Movies page for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be on the home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
--- spec/movies_controller_spec.rb
+++ spec/movies_controller_spec.rb
@@ -35,3 +35,25 @@ describe MoviesController, :type => :controller do
 
   
 end
+
+=begin
+describe MoviesController, :type => :controller do
+  describe 'adding director for an existing movie'
+    it 'when I go to the edit page for Alien,
+      
+    it 'And I fill in "Director" with "Ridley Scott"
+    it 'And I press update Movie Info'
+    it 'Then the director for "Alien" Should be "Ridley Scott"
+  desctibe 'Find a movie with same director'
+    it 'When I am on the details page of "Star Wars"'
+    it 'When I follow "Find Movies With Same Director"
+      response.should 
+    it 'And I should see "THX-1138"'
+    it 'But I should not see "Blade Runner"
+  describe 'What the program does when we don't know the director'
+    it 'When I am on the details page for the movie "Alien"'
+    it Then I should not see "Ridley Scott"
+    it when I follow "Fing Movies With Same direcor"'
+    it "Then I should be on the homepage"
+    it "And I should see "'Alien' Has no director info"
+=end
